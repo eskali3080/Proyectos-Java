12c9ebca3f887f00f243f4d61ffaa3a5276f5720
eskali3080 <143544027+eskali3080@users.noreply.github.com>
2024-01-24 14:34:10 -0500

    Add files via upload

---
+++ Proyecto1.0/java/proyecto1/ProyectoJava1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto1;
+
+/**
+ *
+ * @author Instructor
+ */
+public class ProyectoJava1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
