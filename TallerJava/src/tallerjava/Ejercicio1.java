30f9cebef017b55d6054d4b2f7a461052e5a4ca0
eskali3080 <143544027+eskali3080@users.noreply.github.com>
2024-01-29 16:27:22 -0500

    Add files via upload

---
+++ TallerJava/src/tallerjava/Ejercicio1.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tallerjava;
+
+/**
+ *
+ * @author Instructor
+ */
+public class Ejercicio1 {
+    
+    private long x;
+    private long z;
+    
+    public Ejercicio1(long x, long z) {
+        this.x = x;
+        this.z = z;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public long getZ() {
+        return z;
+    }
+
+    public void setZ(long z) {
+        this.z = z;
+    }
+
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ TallerJava/src/tallerjava/Ejercicio3.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tallerjava;
+
+/**
+ *
+ * @author Instructor
+ */
+public class Ejercicio3 {
+    private long n;
+
+    public Ejercicio3(long n) {
+        this.n = n;
+    }
+
+    public long getN() {
+        return n;
+    }
+
+    public void setN(long n) {
+        this.n = n;
+    }
+   
+    
+}
